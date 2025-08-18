07506d73edae0bc8832ac64a7e47ffb82bb06272
aoosio <mortada@docrep.co>
2025-08-18 00:13:43 +0000

    ruinssss

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }; // no output:'export'
+const nextConfig = { reactStrictMode: true }; // ❌ no output:'export'
 export default nextConfig;
